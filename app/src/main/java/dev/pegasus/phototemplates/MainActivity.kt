41347f6e2feb4972adfdf9b6a7832dc44a172044
Sohaib Ahmed <sohaibahmed.hypersoft@gmail.com>
2023-10-03 10:23:57 +0500

    Photo Templates

--- app/src/main/java/dev/pegasus/phototemplates/MainActivity.kt
+++ app/src/main/java/dev/pegasus/phototemplates/MainActivity.kt
@@ -1,11 +1,15 @@
 package dev.pegasus.phototemplates
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dev.pegasus.phototemplates.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
